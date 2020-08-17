feb5b602a12c70362b5421cedf4244e2af02003d
WDanielH <warrendh@gmail.com>
2020-08-17 00:03:57 -0400

    returned results as expected

--- css/JS/main.js
+++ css/JS/main.js
@@ -3,6 +3,11 @@ var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=yYK5
 xhr.done(function(data) { console.log("success got data", data); });
 
 
+
+
+
+//$("body").append("<img src=""/>")
+
 })
 
 /*
