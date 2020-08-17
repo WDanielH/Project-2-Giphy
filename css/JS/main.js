a01bcd5cb3825bd356a8b8e029878293951fc7d4
WDanielH <warrendh@gmail.com>
2020-08-17 00:17:45 -0400

    working on displaying results

--- css/JS/main.js
+++ css/JS/main.js
@@ -1,9 +1,24 @@
 $(document).ready(()=>{
 var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
+xhr.done((function(data)) { 
+    
+    console.log("success got data", data); });
 
+    })
 
+});
 
+//currently working code
+//$(document).ready(()=>{
+   // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=5");
+   // xhr.done((function(data)) { 
+        
+        //console.log("success got data", data); });
+    
+       // })
+    
+   // });
+    
 
 
 //$("body").append("<img src=""/>")
