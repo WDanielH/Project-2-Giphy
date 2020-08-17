71e3a184fb3a26e83174f893de55ee326497c764
WDanielH <warrendh@gmail.com>
2020-08-17 17:54:39 -0400

    basic functionality working

--- css/JS/main.js
+++ css/JS/main.js
@@ -1,6 +1,34 @@
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+	q = "ryan gosling"; // search query
+	
+	request = new XMLHttpRequest;
+	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&tag='+q, true);
+	
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400){
+			data = JSON.parse(request.responseText).data.image_url;
+			console.log(data);
+			document.getElementById("giphyme").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
+		} else {
+			console.log('reached giphy, but API returned an error');
+		 }
+	};
+
+	request.onerror = function() {
+		console.log('connection error');
+	};
+
+	request.send();
+});
+
+
+
+/*
+
+
 $(document).ready(()=>{
     var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=1");
     xhr.done(function(data) {
