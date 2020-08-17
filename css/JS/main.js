a0b4e8205775c3126123f1b7be437abfd24658ce
WDanielH <warrendh@gmail.com>
2020-08-17 02:35:59 -0400

    reverted back to what works

--- css/JS/main.js
+++ css/JS/main.js
@@ -1,12 +1,51 @@
 $(document).ready(()=>{
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=5");
-    xhr.done(function(data) { console.log("success got data", data); });
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=1");
+    xhr.done(function(data) {
+    
+    console.log("success got data", data);
+
+
+   
+
+
+});
+})
     
     
-    })
+
+
+
+/*
+var api = "http://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G";
+var query = "&q=ryan+gosling";
+var qty = "&limit=1";
+
+function setup(){
+noCanvas();
+var url = api + apiKey + query;
+loadJSON(url, gotData);
+}
+
+function gotData(giphy){
+   createImg(giphy.data[0].images.original.url)
+}
+/*
+
+
+$(document).ready(()=>{
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=1");
+    xhr.done(function(data) {
     
- 
+    console.log("success got data", data);
+
+
+   
+
+
+});
     
-    //Basic code from GIPHY that should make a search for Ryan Gosling
-    // my api key yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G
     
+    
+})
+    */
