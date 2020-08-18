ab695a24be03b919e89613934c46232ad207ed5a
WDanielH <warrendh@gmail.com>
2020-08-17 23:11:19 -0400

    working version with search instead of random

--- css/JS/main.js
+++ css/JS/main.js
@@ -5,9 +5,70 @@ document.addEventListener('DOMContentLoaded', function () {
      let query =  $("#search").val()
      let quantity = $("#quantity").val()
      //alert(q)  
-        
-           
+     
+     var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=1");
+    xhr.done(function(data) {
+   
+    //console.log("success got data", data);
+    console.log(data.data[0].images.original.url);
+    $("body").append('<center><img src = "'+data.data[0].images.original.url+'"  title="GIF via Giphy"></center>')
+    
+    })
+})
+})
+
+    /*
         request = new XMLHttpRequest;
+	    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=2&rating=pg&tag='+query, true);
+	
+	    request.onload = function() {
+		    if (request.status >= 200 && request.status < 400){
+			    data = JSON.parse(request.responseText).data.image_url;
+			    console.log(data);
+           
+                $("body").append('<center><img src = "'+data+'"  title="GIF via Giphy"></center>')
+             
+            } 
+            else {
+			    console.log('reached giphy, but API returned an error');
+		    }
+        }
+    
+	request.onerror = function() {
+		console.log('connection error');
+	};
+
+    request.send();
+ 
+    });
+	
+});
+
+/*
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    $("#submit").click(()=>{
+     let query =  $("#search").val()
+     let quantity = $("#quantity").val()
+     //alert(q)  
+        
+     $.ajax({
+        url:'http://api.giphy.com/v1/gifs/random?api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=2&rating=pg&tag=${query}'
+     
+    })      
+       .done((res)=>{
+           let gifs = res.Search;
+           $.each(gifs, (i, e)=>{
+             console.log("success got data", data);
+             console.log("index",i,"gif",e.image_url)
+           })
+       })
+    })
+})
+
+    /*
+     request = new XMLHttpRequest;
 	    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=2&rating=pg&tag='+query, true);
 	
 	    request.onload = function() {
@@ -95,7 +156,4 @@ $(document).ready(()=>{
 
 });
     
-    
-    
-})
     */
