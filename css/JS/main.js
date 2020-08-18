f9a4bb4910799dca652acb406c8032d2e5cad1aa
WDanielH <warrendh@gmail.com>
2020-08-17 22:38:36 -0400

    played with different implementations, not quite resolved, fixed so that we have basic functionality

--- css/JS/main.js
+++ css/JS/main.js
@@ -1,40 +1,35 @@
 
-
-
 document.addEventListener('DOMContentLoaded', function () {
     
     $("#submit").click(()=>{
      let query =  $("#search").val()
      let quantity = $("#quantity").val()
      //alert(q)  
-    
-    
+        
+           
         request = new XMLHttpRequest;
-	    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&rating=pg&tag='+query, true);
+	    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=yYK5jv0CIzQ0VMfIgccWfzvlsGY3yD3G&limit=2&rating=pg&tag='+query, true);
 	
 	    request.onload = function() {
 		    if (request.status >= 200 && request.status < 400){
 			    data = JSON.parse(request.responseText).data.image_url;
 			    console.log(data);
-            //document.getElementById("giphyme").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
+           
                 $("body").append('<center><img src = "'+data+'"  title="GIF via Giphy"></center>')
+             
             } 
             else {
 			    console.log('reached giphy, but API returned an error');
 		    }
         }
-   
-
+    
 	request.onerror = function() {
 		console.log('connection error');
 	};
 
-	request.send();
-
+    request.send();
+ 
     });
-
-    //q = "ryan gosling"; // search query
-	
 	
 });
 
