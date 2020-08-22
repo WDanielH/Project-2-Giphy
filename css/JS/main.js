20ce2eab7638bd4bdfb4ec7824dcb9cc9722fe0d
WDanielH <warrendh@gmail.com>
2020-08-21 23:56:16 -0400

    basic styling

--- css/JS/main.js
+++ css/JS/main.js
@@ -13,16 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
    
       console.log(data.data[0].images.original.url);
     for(i=0;i<quantity;i=i+3){
-        //$("#results").append('<img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy">')
-        $("#resultsL").append('<div class="grid-item"><img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy"></div>')
+        //$("#resultsL").append('<img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy">')
+        $("#resultsL").append('<div class="grid-item"><center><img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy"></center></div>')
       }
       for(i=1;i<quantity;i=i+3){
-        //$("#results").append('<img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy">')
-        $("#resultsM").append('<div class="grid-item"><img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy"></div>')
+        //$("#resultsM").append('<img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy">')
+        $("#resultsM").append('<div class="grid-item"><center><img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy"></center></div>')
       }
       for(i=2;i<quantity;i=i+3){
-        //$("#results").append('<img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy">')
-        $("#resultsR").append('<div class="grid-item"><img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy"></div>')
+        //$("#resultsR").append('<img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy">')
+        $("#resultsR").append('<div class="grid-item"><center><img src = "'+data.data[i].images.original.url+'"  title="GIF via Giphy"></center></div>')
       }
    
    
